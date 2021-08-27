0714c0f6198a71a9a7e41899b42eaded2c7161f3
MarianaES <mariana.enriquez.s@gmail.com>
2021-08-27 17:59:02 -0500

    JavaScript Tools

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: "Koly",
+      name: "Koly (Gorda preciosa)",
       animal: "Dog",
       breed: "Beagle/Australian Cattle",
     }),
