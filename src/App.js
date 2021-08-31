6261ef90f224e06eca7c8a49cc6b53e67ecadd88
MarianaES <mariana.enriquez.s@gmail.com>
2021-08-30 19:50:26 -0500

    Making work JSXs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+// import React from "react"; Not needed anymore, Babel is smart enogh to identify JSXs
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet
+        name="Koly (Gorda preciosa)"
+        animal="Dog"
+        breed="Beagle/Australian Cattle"
+      />
+      <Pet name="Agaporni" animal="Bird" breed="Agaporni" />
+      <Pet name="Beethoven" animal="Dog" breed="St. Bernard" />
+    </div>
+  );
+};
+
+/* Output after Babel and Parcel 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
@@ -22,5 +38,7 @@ const App = () => {
     }),
   ]);
 };
+*/
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,5 +1,16 @@
-import React from "react";
+// import React from "react"; Not needed anymore, Babel is smart enogh to identify JSXs
 
+const Pet = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  );
+};
+
+/* Output after Babel and Parcel 
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, props.name),
@@ -7,5 +18,5 @@ const Pet = (props) => {
     React.createElement("h3", {}, props.breed),
   ]);
 };
-
+*/
 export default Pet;
