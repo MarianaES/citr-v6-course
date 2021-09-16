793f3be81411ca391d2e4eecfaa35138a7aa411e
MarianaES <mariana.enriquez.s@gmail.com>
2021-09-16 13:34:52 -0500

    Create a Class component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,40 @@
-const Details = () => {
-  return <h2>hi LOL OMG WTF!</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+  // Class components have something called life cycle methods
+  //  Called when react component is render for the 1st time
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    // console.log(this.state);
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// const Details = () => {
+//   return <h2>hi LOL OMG WTF!</h2>;
+// };
+
+export default withRouter(Details);
