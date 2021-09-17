16fef4e11eb8f29987a0d8f4dcd444db7530972c
MarianaES <mariana.enriquez.s@gmail.com>
2021-09-17 17:17:10 -0500

    Implement Modals

--- src/Details.js
+++ src/Details.js
@@ -3,6 +3,8 @@ import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
+
 class Details extends Component {
   // constructor() {
   //   super();
@@ -10,7 +12,7 @@ class Details extends Component {
   //   this.state = { loading: true };
   // }
   // After npm i -D @babel/plugin-proposal-class-properties@7.13.0 @babel/preset-env@7.13.5 @babel/eslint-parser@7.13.4
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   // Class components have something called life cycle methods
   //  Called when react component is render for the 1st time
   async componentDidMount() {
@@ -21,11 +23,14 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => (window.location = "http://bit.ly/pet-adopt");
+
   render() {
     if (this.state.loading) {
       return <h1>Loading... </h1>;
     }
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
     // console.log(this.state);
     return (
@@ -36,12 +41,26 @@ class Details extends Component {
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
           <ThemeContext.Consumer>
             {([themeHook]) => (
-              <button style={{ backgroundColor: themeHook }}>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: themeHook }}
+              >
                 Adopt {name}
               </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
